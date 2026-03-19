3c3db2017624cd016cc80ec130f5fbc2fc7a8f81
manjulsingh261 <manjulsingh261@gmail.com>
2026-03-19 23:40:43 +0530

    maze start
    carve and drawing the maze

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+# define row 5
+# define col 7 
+void carvemaze(){
+
+}
+int main(){
+int mat [row][col];
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            mat[i][j]='#';
+        }
+    }
+    drawmaxe()
+    carvemaze();
+}
